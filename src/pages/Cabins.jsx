c766a5e3605ddfd795d22fb5ba9e05b14c5c953b
deepak <deepaksebak4@gmail.com>
2024-03-01 20:02:28 +0530

    setting up react querry

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Spinner from "../ui/Spinner";
 import { getCabins } from "../services/apiCabins";
+import { useQuery } from "@tanstack/react-query";
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
+  const { data, isLoading, error } = useQuery({
+    queryFn: getCabins,
+  });
+  if (isLoading) return <Spinner />;
   return (
     <>
       <Row type="horizontal">
