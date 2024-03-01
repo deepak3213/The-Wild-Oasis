5f5c10278a682e2aa7fbd0a887ca304c414b9474
deepak <deepaksebak4@gmail.com>
2024-03-01 16:01:06 +0530

    introduction to styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
 function App() {
+  const H1 = styled.h1`
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+      "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    font-size: 5rem;
+    text-align: center;
+  `;
+
+  const Input = styled.input`
+    padding: 1rem;
+    outline: none;
+    border: 1px solid black;
+    font-size: 1rem;
+    &:focus() {
+      outline: 1px solid red;
+    }
+  `;
   return (
     <div>
-      <h1>Hello world</h1>
+      <H1>Hello world</H1>
+      <Input type="text" placeholder="Enter your name.." />
     </div>
   );
 }
