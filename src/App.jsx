cbf4f1e29af7c1b3289eddb47c4c64c209c74973
deepak <deepaksebak4@gmail.com>
2024-03-01 16:12:29 +0530

    add global style with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
-  const H1 = styled.h1`
-    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-      "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-    font-size: 5rem;
-    text-align: center;
-  `;
-
-  const Input = styled.input`
-    padding: 1rem;
-    outline: none;
-    border: 1px solid black;
-    font-size: 1rem;
-    &:focus() {
-      outline: 1px solid red;
-    }
-  `;
   return (
-    <div>
-      <H1>Hello world</H1>
-      <Input type="text" placeholder="Enter your name.." />
-    </div>
+    <>
+      <GlobalStyles />
+      <div>Hello world</div>
+    </>
   );
 }
 
