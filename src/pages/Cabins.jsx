8fabfb94dc814beed7c56f32987718430ef8802b
deepak <deepaksebak4@gmail.com>
2024-03-01 19:33:52 +0530

    connected to the supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-
+import { getCabins } from "../services/apiCabins";
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
+      </Row>
+      <h1>{import.meta.env.supabaseKey}</h1>
+    </>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://stngepvjwdpybmrxuyan.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+// ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0bmdlcHZqd2RweWJtcnh1eWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM2MjI2MTEsImV4cCI6MjAwOTE5ODYxMX0.5WYp4fQUta7o-wj_saSoXIFGk_6ZZN8RyyMyO1GVh4U");
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
